e6856ded1f407e856e3784cb8da2e364d242ae5c
Eryu Suo <suoeryu@icloud.com>
2017-12-04 15:34:50 -0800

    Roughly complete functionality

--- app.py
+++ app.py
@@ -1,12 +1,107 @@
-from flask import Flask
+import os
+import uuid
+
+import matplotlib
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+import database_manager as dm
+
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+app.config['IMG_FOLDER'] = 'static/images'
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', datasets=dm.get_data_sets())
+
+
+@app.route('/init_database')
+def init_database():
+    dm.init_database()
+    return "Success to init database"
+
+
+@app.route('/upload_csv', methods=['GET', 'POST'])
+def upload_csv():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    else:
+        if 'csv_file' not in request.files:
+            app.logger.debug("NO File part")
+            flash('No file part')
+            return redirect(url_for('upload_csv'))
+        csv_file = request.files['csv_file']
+        if csv_file.filename == '':
+            flash('No selected file!')
+            return redirect(url_for('upload_csv'))
+        if csv_file.filename.rsplit('.', 1)[1].lower() != 'csv':
+            flash('Uploaded file should be csv type')
+            return redirect(url_for('upload_csv'))
+        data_set_name = request.form['DataSetName'].strip()
+        data_set_name.replace(' ', '_')
+        if data_set_name == '':
+            flash('Data set name is Empty!')
+            return redirect(url_for('upload_csv'))
+        if dm.exist_data_set(data_set_name):
+            flash('{} is exists!'.format(data_set_name))
+            return redirect(url_for('upload_csv'))
+        app.logger.debug(data_set_name)
+        info = dm.add_dataset(data_set_name, csv_file)
+        app.logger.debug(info.describe())
+        return redirect(url_for('index'))
+
+
+@app.route('/del_dataset/<name>')
+def delete_data_set(name):
+    dm.delete_data_set(name)
+    return redirect(url_for('index'))
+
+
+@app.route('/dataset/<name>')
+def view_data_set(name):
+    return render_template('dataset.html', table_name=name, col_infos=dm.get_col_infos(name))
+
+
+@app.route('/column/<table_name>/<col_name>')
+def view_column(table_name, col_name):
+    param_dict = {
+        'table_name': table_name,
+        'col_name': col_name,
+        'col_info': dm.get_col_info(table_name, col_name),
+        'col_infos': dm.get_col_infos(table_name),
+        'images': dm.get_all_images(table_name, col_name),
+    }
+    return render_template('column.html', **param_dict)
+
+
+@app.route('/add_figure', methods=['POST'])
+def add_figure():
+    fig_folder = app.config['IMG_FOLDER']
+    fig_name = str(uuid.uuid4())
+    table_name = request.form['table_name']
+    col_name = request.form['col_name']
+    col_values = dm.get_col_values(table_name, col_name)
+    fig_title = request.form['fig_title']
+    plt.figure()
+    if request.form['fig_type'] == 'hist':
+        plt.hist(col_values)
+    else:
+        rel_col_name = request.form['rel_column']
+        rel_col_values = dm.get_col_values(table_name, rel_col_name)
+        plt.plot(col_values, rel_col_values, 'bo')
+        plt.xlabel(col_name)
+        plt.ylabel(rel_col_name)
+    plt.savefig(os.path.join(fig_folder, fig_name))
+    dm.insert_image(fig_name, table_name, col_name, fig_title)
+    return redirect(url_for('view_column', table_name=table_name, col_name=col_name))
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    # sess.init_app(app)
     app.run(debug=True, host="0.0.0.0")
---
+++ database_manager.py
@@ -0,0 +1,157 @@
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+from datetime import datetime
+
+import pandas as pd
+from cassandra.query import tuple_factory, BatchStatement
+
+cluster = Cluster(['database'])
+keyspace = 'cpsc531'
+
+
+def get_session(ks=keyspace):
+    return cluster.connect(keyspace=ks)
+    # if keyspace:
+    #     if not hasattr(get_session, 'session_dict'):
+    #         get_session.session_dict = dict()
+    #     if keyspace not in get_session.session_dict:
+    #         s = cluster.connect(keyspace)
+    #         get_session.session_dict[keyspace] = s
+    #         return s
+    #     else:
+    #         return get_session.session_dict.get(keyspace)
+    # else:
+    #     if not hasattr(get_session, 'session'):
+    #         get_session.session = cluster.connect()
+    #     return get_session.session
+
+
+def init_database():
+    session = get_session(None)
+    session.execute("drop KEYSPACE if exists %s;" % keyspace)
+
+    session.execute(
+        """CREATE KEYSPACE %s WITH REPLICATION = {
+                'class' : 'SimpleStrategy',
+                'replication_factor' : %%s
+        };""" % keyspace,
+        (1,)
+    )
+    session.set_keyspace('cpsc531')
+    session.execute(
+        '''CREATE TABLE DATASETS(orig_table text, target_table text, create_time timestamp,
+                                 PRIMARY KEY (orig_table));''',
+    )
+    session.execute(
+        '''CREATE TABLE DATASETINFO(table_name text, col_name text, col_type text,
+                                     min_val double, max_val double, mean_val double,
+                                     PRIMARY KEY(table_name, col_name))'''
+    )
+    session.execute(
+        '''CREATE TABLE IMAGES(fig_name text, table_name text, col_name text,
+                               fig_title text, create_time timestamp,
+                               PRIMARY KEY(fig_name, table_name, col_name))'''
+    )
+
+
+def add_dataset(name, csv_file):
+    name = name.upper()
+    session = get_session()
+    info = pd.read_csv(csv_file, index_col=False)
+    session.execute('CREATE TABLE {} (idx int, {}, PRIMARY KEY (idx))'.format(name, ', '.join(
+        [col_name + (' double' if col_type == float else ' text')
+         for col_name, col_type in zip(info.columns, info.dtypes)])))
+    session.execute('CREATE TABLE {} (idx int, PRIMARY KEY(idx))'.format(name + '_TARGET'))
+    session.execute(
+        "INSERT INTO DATASETS (orig_table, target_table, create_time) VALUES (%s, %s, %s)",
+        [name, name + "_TARGET", datetime.now()]
+    )
+    insert_data = session.prepare(
+        'INSERT INTO {} (idx,{}) VALUES (?,{})'.format(name, ','.join(info.columns),
+                                                       ','.join(['?'] * len(info.columns))))
+    batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
+    for idx, row in info.iterrows():
+        values = list(row.values)
+        values.insert(0, idx)
+        batch.add(insert_data, tuple(values))
+        if (idx + 1) % 20 == 0:
+            session.execute(batch)
+            batch.clear()
+    session.execute(batch)
+    insert_col_info = session.prepare(
+        '''INSERT INTO DATASETINFO (table_name, col_name, col_type, min_val, max_val, mean_val)
+                            VALUES (?,          ?,        ?,        ?,       ?,       ?)'''
+    )
+    batch = BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
+    for col_name, col_type in zip(info.columns, info.dtypes):
+        col_data = info[col_name]
+        if col_type == float:
+            batch.add(insert_col_info,
+                      (name, col_name, 'DOUBLE', col_data.min(), col_data.max(), col_data.mean()))
+        else:
+            batch.add(insert_col_info, (name, col_name, 'STRING', None, None, None))
+    session.execute(batch)
+    return info
+
+
+def exist_data_set(name):
+    sess = get_session()
+    sess.row_factory = tuple_factory
+    rows = sess.execute("SELECT orig_table FROM DATASETS where orig_table = %s", [name])
+    return len(list(rows))
+
+
+def get_data_sets():
+    sess = get_session()
+    rows = sess.execute("SELECT orig_table, target_table, create_time FROM DATASETS")
+    return list(rows)
+
+
+def delete_data_set(name):
+    sess = get_session()
+    sess.execute("DELETE from DATASETS where orig_table = %s", [name])
+    sess.execute('DROP TABLE IF EXISTS {}'.format(name))
+    sess.execute('DROP TABLE IF EXISTS {}_TARGET'.format(name))
+    return None
+
+
+def get_col_infos(table_name):
+    sess = get_session()
+    rows = sess.execute(
+        '''SELECT col_name, col_type, min_val, max_val, mean_val
+              FROM DATASETINFO
+           WHERE TABLE_NAME = %s
+        ''', [table_name])
+    return list(rows)
+
+
+def get_col_info(table_name, col_name):
+    sess = get_session()
+    rows = sess.execute(
+        '''SELECT col_type, min_val, max_val, mean_val
+              FROM DATASETINFO
+           WHERE TABLE_NAME = %s and COL_NAME = %s''',
+        [table_name, col_name])
+    return rows[0]
+
+
+def get_col_values(table_name, col_name):
+    sess = get_session()
+    rows = sess.execute('SELECT {} FROM {}'.format(col_name, table_name))
+    return [row[0] for row in rows]
+
+
+def insert_image(fig_name, table_name, col_name, fig_title):
+    sess = get_session()
+    sess.execute('''INSERT INTO IMAGES(fig_name, table_name, col_name, fig_title, create_time)
+                                VALUES (%s, %s, %s, %s, %s)''',
+                 [fig_name, table_name, col_name, fig_title, datetime.now()])
+    return None
+
+
+def get_all_images(table_name, col_name):
+    sess = get_session()
+    rows = sess.execute('''SELECT fig_title, fig_name, create_time FROM IMAGES
+                             WHERE table_name = %s and col_name = %s ALLOW FILTERING''',
+                        [table_name, col_name])
+    return list(rows)
